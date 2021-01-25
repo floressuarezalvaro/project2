9d7e3792f0469acc5bff545473e51206e2422319
Alvy <floressuarezalvaro@gmail.com>
2021-01-24 21:50:52 -0800

    CRUD for users

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -4,9 +4,23 @@ var axios = require("axios");
 require("dotenv").config();
 
 module.exports = (app) => {
-  app.post("/api/login", passport.authenticate("local"), (req, res) => {
-    res.json(`This user has been logged ${req.user}`);
-  });
+  // app.get("/api/users/:userEmail", (req, res) => {
+  //   if (!req.user) {
+  //     res.json({
+  //       email: "No user email",
+  //       id: "No IDs",
+  //     });
+  //   } else {
+  //     res.json({
+  //       email: req.user.email,
+  //       id: req.user.id,
+  //     });
+  //   }
+  // });
+
+  // app.post("/api/login", passport.authenticate("local"), (req, res) => {
+  //   res.json(`This user has been logged ${req.user}`);
+  // });
 
   app.post("/api/users/createAccount", (req, res) => {
     db.User.create({
@@ -21,22 +35,31 @@ module.exports = (app) => {
     //   res.status(401).json(err);
     // });
   });
+  app.get("/api/users/:id", (req, res) => {
+    db.Userc.findAll({
+      where: {
+        id: req.params.id,
+      },
+    }).then((dbGetUser) => res.json(dbGetUser));
+  });
+
+  app.put("/api/users/updateAccount/:id", (req, res) => {
+    db.User.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    }).then((dbUpdateUser) => res.json(dbUpdateUser));
+  });
+
+  app.delete("/api/users/deleteAccount/:id", (req, res) => {
+    db.User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    }).then((dbDestroyUser) => res.json(dbDestroyUser));
+  });
 
   // place logout here
-
-  app.get("/api/users/user_info", (req, res) => {
-    if (!req.user) {
-      res.json({
-        email: "No user email",
-        id: "No IDs",
-      });
-    } else {
-      res.json({
-        email: req.user.email,
-        id: req.user.id,
-      });
-    }
-  });
 
   app.get("/api/searchLoc", (req, res) => {
     let searchLocation = req.query.query;
