fee823c08e672d7bc75e4f345f9464d5334e580f
bracamon <bracamontes.ochoa@gmail.com>
2021-01-24 22:52:26 -0700

    define barCards function

--- public/JS/public.js
+++ public/JS/public.js
@@ -1,3 +1,9 @@
 let searchFunction = require("searchFunction");
 
-let barCards = function (data) {};
+let barCards = function (data) {
+  fetchSearchLocation().then((data) => {
+    $("#bar-name").text("Brewery Name: " + data[0].name);
+    $("#city-name").text("City Name: " + data[0].city);
+    $("#state").text("State: " + data[0].state);
+  });
+};
