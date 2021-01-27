596caed34a6adf6028756c587e6cbb0d60d8b69a
Alvy <floressuarezalvaro@gmail.com>
2021-01-27 13:34:44 -0800

    redirecting bar post to index

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,7 +6,7 @@ module.exports = (app) => {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads index.html
-  app.get("/home", (req, res) => {
+  app.get("/index", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/HTML/index.html"));
   });
   // login route loads login.html
