2a00dbbf536057e807707de59d43138f86c3323d
Nicolas Martinez <Nimarti548@gmail.com>
2021-01-26 20:40:19 -0800

    UPDATE:Fixed the modals

--- public/JS/search.js
+++ public/JS/search.js
@@ -1,5 +1,7 @@
 let searchForm = document.querySelector("#search-form");
 
+let searchResult = []
+
 function fetchSearchLocation(queryString) {
   if (!queryString || queryString.trim() === "") {
     return;
@@ -14,11 +16,12 @@ function fetchSearchLocation(queryString) {
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-      $("#barCard").empty();
+      searchResult = data;
+      console.log(searchResult);
+      $("#barList").empty();
       for (let i = 0; i < data.length; i++) {
         //create barCards here
-        // let row = $("<div>").addClass("row");
+        
         let col = $("<div>").addClass("col s12 m4 l4");
         let card = $("<div>").addClass("card");
         let body = $("<div>").addClass("card-content");
@@ -32,38 +35,76 @@ function fetchSearchLocation(queryString) {
         let state = $("<p>")
           .addClass("#state")
           .text("State: " + data[i].state);
-        let action = $("<div>").addClass("card-action");
-        let modal = $("<a>")
-          .addClass("waves-effect waves-light btn modal-trigger")
-          .attr("href", "#modal1")
-          .text("More Details");
-        let modal1 = $("<div>").addClass(".modal").attr("id", "modal1");
-        $(document).ready(function () {
-          $(".modal").modal();
-        });
-
-        let modalContent = $("<div>").addClass(".modal-content")
-        let h4 = $("<h4>").text("Popular Brewery Info");
-        let p1 = $("<p>").addClass("#bar-name").text("Name: " + data[i].name);
-        let p2 = $("<p>").addClass("#address").text("Address: " + data[i].street);
-        let p3 = $("<p>").addClass("#city-name").text("City: " + data[i].city);
-        let p4 = $("<p>").addClass("#state").text("State: " + data[i].state);
-        let p5 = $("<p>").addClass("#review-link").text("Reviews: " + data[i].reviewlink);
-        let p6 = $("<p>").addClass("#phone").text("Phone Number: " + data[i].phone);
-        let p7 = $("<p>").addClass("#website").text("Website: " + data[i].url);
+          let action = $("<div>").addClass("card-action");
+let modal = $("<a>")
+  .addClass("waves-effect waves-light btn modal-trigger")
+  .attr('data-id', i)
+  .attr("href", "#modal1")
+  .text("More Details");
+        
 
         // merge together and put on page
         
           col.append(
-            card.append(body.append(btn, name, city, state, action, modal, modal1.append(modalContent, h4, p1, p2, p3, p4, p5, p6, p7))
+            card.append(body.append(btn, name, city, state, action, modal)
           )
         );
-        $("#barCard").append(col);
+        $("#barList").append(col);
 
         
       }
     });
 }
+// let modal1 = $("div.modal")
+
+function modalInfo(index) {
+
+  $("#bar-name").text(`Bar Name: ${searchResult[index].name}`);
+  $("#address").text(`Address: ${searchResult[index].street}`);
+  $("#city").text(`City: ${searchResult[index].city}`);
+  $("#state").text(`State: ${searchResult[index].state}`);
+  $("#review-link").text(`Review Link: ${searchResult[index].reviewlink}`);
+  $("#phone").text(`Phone Number: ${searchResult[index].phone}`);
+  $("#website").text(`Website: ${searchResult[index].url}`);
+}
+
+$(document).ready(function () {
+  $("#modal1").modal();
+ 
+  $(document).on("click", ".modal-trigger", function(e) {
+    // $("#modal1").modal();
+    let index = e.currentTarget.getAttribute("data-id");
+    modalInfo(index)
+  });
+});
+
+//  let modalContent = $("<div>").addClass(".modal-content");
+//  let h4 = $("<h4>").text("Popular Brewery Info");
+//  let p1 = $("<p>")
+//    .addClass("#bar-name")
+//    .text("Name: ");// + data[i].name);
+//  let p2 = $("<p>")
+//    .addClass("#address")
+//    .text("Address: "); // + data[i].street);
+//  let p3 = $("<p>")
+//    .addClass("#city-name")
+//    .text("City: ");// + data[i].city);
+//  let p4 = $("<p>")
+//    .addClass("#state")
+//    .text("State: "); // + data[i].state);
+//  let p5 = $("<p>")
+//    .addClass("#review-link")
+//    .text("Reviews: "); // + data[i].reviewlink);
+//  let p6 = $("<p>")
+//    .addClass("#phone")
+//    .text("Phone Number: "); // + data[i].phone);
+//  let p7 = $("<p>")
+//    .addClass("#website")
+//    .text("Website: "); // + data[i].url);
+
+
+//    modal1.append(modalContent, h4, p1, p2, p3, p4, p5, p6, p7);
+
 
 function handleSearchFormSubmit(e) {
   e.preventDefault();
